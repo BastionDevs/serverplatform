48a4e83137d1930c2e2e13ecf8ba9c8813b04a56
CubeNotFound <itsandrewplayz@hotmail.com>
2025-04-09 09:52:41 +0800

    Added project information class.

---
+++ src/ServerPlatform/serverplatform/ProjectInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverplatform
+{
+    internal class ProjectInfo
+    {
+
+        /*
+            ____             __  _             ___________
+           / __ )____ ______/ /_(_)___  ____  / ____/ ___/
+          / __  / __ `/ ___/ __/ / __ \/ __ \/ /    \__ \
+         / /_/ / /_/ (__  ) /_/ / /_/ / / / / /___ ___/ /
+        /_____/\__,_/____/\__/_/\____/_/ /_/\____//____/
+
+        Project Information Standardisation
+        */
+
+        static string ProjectName = "ServerPlatform",
+            ProjectVersion = "1.0",
+            ReleaseChannel = "STABLE";
+
+    }
+}
