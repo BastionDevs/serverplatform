afcf00465ecef283b6e94dd0165d6ac191ecb3f4
CubeNotFound <itsandrewplayz@hotmail.com>
2025-04-09 12:54:19 +0800

    Function for checking SHA256 hash of file. Can be used to verify integrity of downloaded files.

---
+++ src/ServerPlatform/serverplatform/ServerCreation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverplatform
+{
+    internal class ServerCreation
+    {
+        public static string CalculateSHA256Hash(string filePath)
+        {
+            using (var sha256 = SHA256.Create())  // Use SHA256 hashing algorithm
+            using (var fileStream = File.OpenRead(filePath))
+            {
+                var hashBytes = sha256.ComputeHash(fileStream);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();  // Convert to a lowercase hex string
+            }
+        }
+    }
+}
