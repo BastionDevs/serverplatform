0ba4aadbe482595b13f8507808a47dfa5129f44b
Andrew <itsandrewplayz@hotmail.com>
2025-04-26 22:28:19 +0800

    Class to get Backend Configuration

---
+++ src/ServerPlatform/serverplatform/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyINIController;
+
+namespace serverplatform
+{
+    internal class Config
+    {
+
+        static IniFile spCnfFile= new IniFile("config.ini");
+
+        public static string GetConfig(string key, string section)
+        {
+            return spCnfFile.Read(key, section);
+        }
+
+    }
+}
