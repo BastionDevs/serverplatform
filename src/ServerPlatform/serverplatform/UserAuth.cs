2c968f9d369a0e1778c96b2b40696e8725eac3a1
CubeNotFound <itsandrewplayz@hotmail.com>
2025-05-08 10:35:35 +0800

    Function for default no dash

--- src/ServerPlatform/serverplatform/UserAuth.cs
+++ src/ServerPlatform/serverplatform/UserAuth.cs
@@ -37,5 +37,14 @@ namespace serverplatform
                 }
             }
         }
+        public static string SHA256Hash(string password)
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                byte[] hash = sha.ComputeHash(bytes);
+                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant()
+            }
+        }
     }
 }
