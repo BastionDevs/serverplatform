586c3df3dc1b77131e194d54cd191faaa862fa6e
CubeNotFound <itsandrewplayz@hotmail.com>
2025-05-08 10:34:48 +0800

    Added function for hashing a string with SHA256

--- src/ServerPlatform/serverplatform/UserAuth.cs
+++ src/ServerPlatform/serverplatform/UserAuth.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,23 @@ namespace serverplatform
             sw.WriteLine("        \"PasswordHash\": \"240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9\"");
             sw.WriteLine("    }");
             sw.WriteLine("]");
+        }
+
+        public static string SHA256Hash(string password, bool nodash)
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                byte[] hash = sha.ComputeHash(bytes);
+
+                if (nodash)
+                {
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                } else
+                {
+                    return BitConverter.ToString(hash).ToLowerInvariant();
+                }
+            }
         }
     }
 }
