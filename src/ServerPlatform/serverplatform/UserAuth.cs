273f3c20021bb63a9845658e8cdd3cfefcdfdc80
CubeNotFound <itsandrewplayz@hotmail.com>
2025-05-08 22:39:27 +0800

    Random alphanumeric string generator

--- src/ServerPlatform/serverplatform/UserAuth.cs
+++ src/ServerPlatform/serverplatform/UserAuth.cs
@@ -46,5 +46,14 @@ namespace serverplatform
                 return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
             }
         }
+
+        //Credit https://github.com/tylerablake/randomStringGenerator
+        //To generate a random alphanumeric string of a specified length
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray()).ToLower();
+        }
     }
 }
