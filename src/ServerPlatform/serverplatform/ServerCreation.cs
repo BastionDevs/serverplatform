d34d591d9b29d672def7c3216e22e4e88419e75c
CubeNotFound <cubenotfound@outlook.sg>
2025-05-23 11:07:24 +0800

    uh oh @echo off isnt a bash script

--- src/ServerPlatform/serverplatform/ServerCreation.cs
+++ src/ServerPlatform/serverplatform/ServerCreation.cs
@@ -66,7 +66,7 @@ namespace serverplatform
                     string jarFileName = Path.GetFileName(new Uri(serverJarUrl).AbsolutePath);
                     new WebClient().DownloadFile(serverJarUrl, $@"{serverDirectory}\\{jarFileName}");
 
-                    File.WriteAllText($@"{serverDirectory}\paper.sh", $"@echo off\r\njava -Xms{ramAmounts[0]}M -Xmx{ramAmounts[1]}M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar {jarFileName} nogui\r\npause");
+                    File.WriteAllText($@"{serverDirectory}\paper.bat", $"@echo off\r\njava -Xms{ramAmounts[0]}M -Xmx{ramAmounts[1]}M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar {jarFileName} nogui\r\npause");
                 }
                 else
                 {
@@ -98,7 +98,7 @@ namespace serverplatform
                     string jarFileName = Path.GetFileName(new Uri(serverJarUrl).AbsolutePath);
                     new WebClient().DownloadFile(serverJarUrl, $@"{serverDirectory}\\{jarFileName}");
 
-                    File.WriteAllText($@"{serverDirectory}\velo.sh", $"@echo off\r\njava -Xms{ramAmounts[0]}M -Xmx{ramAmounts[1]}M -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar {jarFileName}");
+                    File.WriteAllText($@"{serverDirectory}\velo.bat", $"@echo off\r\njava -Xms{ramAmounts[0]}M -Xmx{ramAmounts[1]}M -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar {jarFileName}");
                 }
                 else
                 {
