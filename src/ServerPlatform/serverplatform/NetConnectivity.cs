27d6c680c837f5b1732978cce9586bde3e8b309d
Andrew <itsandrewplayz@hotmail.com>
2025-05-28 21:31:44 +0800

    http reachable test

---
+++ src/ServerPlatform/serverplatform/NetConnectivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverplatform
+{
+    internal class NetConnectivity
+    {
+        public static bool CanConnectHttp(string url)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers.Add("User-Agent", "Mozilla/5.0"); // Some servers require this
+                    using (client.OpenRead(url))
+                    {
+                        return true; // Connection succeeded
+                    }
+                }
+            }
+            catch
+            {
+                return false; // Failed to connect
+            }
+        }
+    }
+}
