f2dfb03c1b6a2a78a2d4f8d88377a62ee4f2c268
CubeNotFound <cubenotfound@outlook.sg>
2025-06-16 23:04:49 +0800

    java runtimes ty shi

---
+++ src/ServerPlatform/serverplatform/JavaRuntimes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverplatform
+{
+    internal class JavaRuntimes
+    {
+        public enum JDKDist
+        {
+            Temurin,
+            Zulu,
+            Liberica,
+            Corretto
+        }
+    }
+}
