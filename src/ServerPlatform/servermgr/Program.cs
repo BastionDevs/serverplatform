3c0366867d6442fd488ae6e6c729beaff740d713
CubeNotFound <cubenotfound@outlook.sg>
2025-07-20 17:32:23 +0800

    GUI standalone server manager

--- src/ServerPlatform/servermgr/Program.cs
+++ src/ServerPlatform/servermgr/Program.cs
@@ -1,36 +1,22 @@
 ﻿using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace servermgr
 {
-    internal class Program
+    internal static class Program
     {
-        private static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine(
-                "    ____             __  _             ___________\r\n   / __ )____ ______/ /_(_)___  ____  / ____/ ___/\r\n  / __  / __ `/ ___/ __/ / __ \\/ __ \\/ /    \\__ \\ \r\n / /_/ / /_/ (__  ) /_/ / /_/ / / / / /___ ___/ / \r\n/_____/\\__,_/____/\\__/_/\\____/_/ /_/\\____//____/");
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            Console.WriteLine("Bastion Server Platform");
-            Console.WriteLine("Server Manager");
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("© 2025 Bastion Faculty of Computer Science");
-            Console.WriteLine("https://www.bastionsg.rf.gd");
-
-            Console.WriteLine();
-            Console.WriteLine("Version 1.0");
-
-            Console.Write("[");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("STABLE");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("] Release Channel");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
--- src/ServerPlatform/servermgr/Properties/AssemblyInfo.cs
+++ src/ServerPlatform/servermgr/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2b629de3-5211-44c5-9ba9-ac41fe6fcb0d")]
+[assembly: Guid("cccd0ee4-a6b6-4387-9051-46814f19df88")]
 
 // Version information for an assembly consists of the following four values:
 //
