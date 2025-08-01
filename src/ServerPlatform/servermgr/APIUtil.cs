5ae389274649fc93048624503665aeda6d58db7b
CubeNotFound <cubenotfound@outlook.sg>
2025-08-02 02:53:17 +0800

    POST utils for servermgr

---
+++ src/ServerPlatform/servermgr/APIUtil.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace servermgr
+{
+    internal class APIUtil
+    {
+        public static string PostJson(string url, string jsonBody)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(jsonBody);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response != null)
+                {
+                    using (var streamReader = new StreamReader(ex.Response.GetResponseStream()))
+                    {
+                        string errorResponse = streamReader.ReadToEnd();
+                        return $"Error: {errorResponse}";
+                    }
+                }
+                return $"Exception: {ex.Message}";
+            }
+        }
+
+        public static string PostAuthenticated(string url, string bearerToken, string body = "")
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            if (!string.IsNullOrEmpty(bearerToken))
+            {
+                request.Headers["Authorization"] = "Bearer " + bearerToken;
+            }
+
+            // Write body if present (even if it's just an empty string)
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(body ?? string.Empty);
+            }
+
+            try
+            {
+                using (var response = (HttpWebResponse)request.GetResponse())
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                using (var errorResponse = (HttpWebResponse)ex.Response)
+                using (var reader = new StreamReader(errorResponse.GetResponseStream()))
+                {
+                    string error = reader.ReadToEnd();
+                    return $"Error ({(int)errorResponse.StatusCode}): {error}";
+                }
+            }
+        }
+    }
+}
