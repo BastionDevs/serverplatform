dcf8660f70997f4025e83bc2435791267da27bc8
CubeNotFound <cubenotfound@outlook.sg>
2025-08-03 22:24:45 +0800

    Increase Form2 size

--- src/ServerPlatform/servermgr/Form2.Designer.cs
+++ src/ServerPlatform/servermgr/Form2.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(788, 552);
+            this.ClientSize = new System.Drawing.Size(828, 562);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
