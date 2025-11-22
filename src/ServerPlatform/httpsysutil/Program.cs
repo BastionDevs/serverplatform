75cd14db0880803ae526dc6531ef66b6dbcd6973
CubeNotFound <cubenotfound@outlook.sg>
2025-11-22 15:06:28 +0800

    Default Message

--- src/ServerPlatform/httpsysutil/Program.cs
+++ src/ServerPlatform/httpsysutil/Program.cs
@@ -10,6 +10,15 @@ namespace httpsysutil
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.Title = "Bastion Server Platform | HTTP System Utility";
+                Console.WriteLine("Bastion Server Platform");
+                Console.WriteLine("HTTP System Utility");
+                Console.WriteLine();
+                Console.WriteLine("Missing options. Try 'httpsysutil --help' for more information");
+                Environment.Exit(1);
+            }
         }
     }
 }
